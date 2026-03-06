30482da454c0f8dc91039ece548e85d66d99b4bb
Moon Desarrollos <moondesarrollos@gmail.com>
2026-03-06 00:18:16 -0300

    Fix NameError in main.py: Added Response to FastAPI imports.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Depends, HTTPException, status
+from fastapi import FastAPI, Request, Response, Depends, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, JSONResponse
